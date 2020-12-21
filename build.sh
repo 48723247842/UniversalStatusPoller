f1028a270eec77cfbfa1fd04a2ca9f28faff15da
48723247842 <48723247842@protonmail.com>
2020-12-21 16:13:35 -0500

    3

--- build.sh
+++ build.sh
@@ -13,5 +13,40 @@ go mod init universalstatuspoller
 RMUHash=$(curl -s 'https://api.github.com/repos/0187773933/RedisManagerUtils/git/refs/heads/master' | jq -r '.object.sha')
 go get "github.com/0187773933/RedisManagerUtils/@$RMUHash"
 
-/usr/local/bin/goBuildAllPlatforms mediaboxUniversalStatusPoller
+
+BinaryName="mediaboxUniversalStatusPoller"
+rm -rf ./bin/
+# https://stackoverflow.com/questions/25051623/golang-compile-for-all-platforms-in-windows-7-32-bit
+# https://www.digitalocean.com/community/tutorials/how-to-build-go-executables-for-multiple-platforms-on-ubuntu-16-04
+
+declare -a linux_architectures=(
+	"amd64"
+	"arm"
+	"arm64"
+)
+declare -a darwin_architectures=(
+	"amd64"
+)
+declare -a windows_architectures=(
+	"amd64"
+)
+
+for architecture in "${linux_architectures[@]}"
+do
+	echo "Building Linux: $architecture"
+	GOOS=linux GOARCH=$architecture go build -o bin/linux/$architecture/$BinaryName
+done
+
+for architecture in "${darwin_architectures[@]}"
+do
+	echo "Building Darwin: $architecture"
+	GOOS=darwin GOARCH=$architecture go build -o bin/darwin/$architecture/$BinaryName
+done
+
+for architecture in "${windows_architectures[@]}"
+do
+	echo "Building Windows: $architecture"
+	GOOS=windows GOARCH=$architecture go build -o bin/windows/$architecture/$BinaryName
+done
+
 sudo cp ./bin/linux/amd64/mediaboxUniversalStatusPoller /usr/local/bin/
